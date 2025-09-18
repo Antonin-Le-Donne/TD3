9f08de9c3f265fe9e49244b74472dc51986f27d9
Etudiant <ledonneantoni@gmail.com>
2025-09-18 12:13:20 +0200

    java doc

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -1,19 +1,25 @@
 package com.iutblagnac.r303trace;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println(hello());
     }
 
+    /**
+     * dis "Hello World!"
+     * @return "Hello World!" si pas de paramètre
+     */
     public static String hello() {
         return "Hello World!";
     }
 
+    /**
+     * dis bonjour avec un paramètre
+     * @param param le paramètre
+     * @return le paramètre
+     */
     public static String hello(String param) {
         return param;
     }
